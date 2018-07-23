b0f7d3dd769edec1121c67964e1375c15e19a465
Martin Lescano <tincholescano2@gmail.com>
2018-07-23 16:02:39 -0300

    Update Class_13.sql

--- Class_13.sql
+++ Class_13.sql
@@ -9,28 +9,24 @@ WHERE country.country LIKE 'United States'
 ORDER BY address_id DESC LIMIT 1));
 
 2)
-
+INSERT INTO rental (rental_date, inventory_id, customer_id, staff_id) VALUES(
+    CURRENT_DATE(),
+    (SELECT inventory_id FROM inventory INNER JOIN film USING (film_id) WHERE title = "ACADEMY DINOSAUR" LIMIT 1),
+    (SELECT customer_id FROM customer WHERE store_id = 1 LIMIT 1),
+    (SELECT staff_id FROM staff WHERE store_id = 1 LIMIT 1));
 
 3)
 UPDATE film
-SET release_year = '2001'
-WHERE rating = 'G';
+SET release_year = CASE
+    WHEN rating = 'G' THEN 1970
+    WHEN rating = 'PG' THEN 1971
+    WHEN rating = 'PG-13' THEN 1972
+    WHEN rating = 'R' THEN 1973
+    WHEN rating = 'NC-17' THEN 1974
+END;
 
-UPDATE film
-SET release_year = '2002'
-WHERE rating = 'PG';
+4)
 
-UPDATE film
-SET release_year = '2003'
-WHERE rating = 'PG-13';
-
-UPDATE film
-SET release_year = '2004'
-WHERE rating = 'R';
-
-UPDATE film
-SET release_year = '2005'
-WHERE rating = 'NC-17';
 
 5)
 DELETE FROM film
